462608b0bd6350b0acaa9b19fcdae920f44f45d0
Neka <benjamin.anezo@protonmail.com>
2023-06-09 13:29:18 +0200

    Delete config.js

--- src/utils/config.js
+++
@@ -1,8 +0,0 @@
-// const config = {
-//   API_BASE_URL: 'https://api.example.com',
-//   API_KEY: 'your-api-key',
-// };
-
-//export default config;
-
-export const API_BASE_URL = 'https://globe-scratch-backend.vercel.app';
